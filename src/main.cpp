0c9427a9ab09aa4d7e48bf95b5329cfdef9bde8f
Chip <chipz68q@gmail.com>
2018-10-29 21:20:59 +0500

    Added ncurses support
    Modified configure.ac, src/Makefile.am src/main.cpp for simple ncurses app

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,20 @@
-#include <iostream>
+#include <ncurses.h>
 
 namespace
 {
 	void start()
 	{
-		std::cout << "Welcome to the Online Game!\n";
+		printw("Welcome to the Online Game!\n");
+		refresh();
+		getch();
 	}
 }
 
 int main()
 {
+	initscr();
 	start();
+	endwin();
 
 	return 0;
 }
