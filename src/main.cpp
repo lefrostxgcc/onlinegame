fef87651230d921a3e754634ed7d1d739729b499
Chip <chipz68q@gmail.com>
2018-10-29 21:58:04 +0500

    Lesson2 stage
    Implemented cursor moving by up-down-right-left keys, exit by 'q'.

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,33 @@ namespace
 {
 	void start()
 	{
+		int row{5}, col{7}, key;
+
 		printw("Welcome to the Online Game!\n");
-		refresh();
-		getch();
+
+		while (true)
+		{
+			mvaddch(row, col, 'O');
+			refresh();
+			key = getch();
+			mvaddch(row, col, ' ');
+			switch (key)
+			{
+				case KEY_DOWN	: row++; break;
+				case KEY_UP	: row--; break;
+				case KEY_LEFT	: col--; break;
+				case KEY_RIGHT	: col++; break;
+				case 'q'	: return;
+			}
+		}
 	}
 }
 
 int main()
 {
 	initscr();
+	keypad(stdscr, true);
+	noecho();
 	start();
 	endwin();
 
