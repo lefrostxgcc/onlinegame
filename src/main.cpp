646039f5c5b7f65da036222affe1d47731d5ce5c
Chip <chipz68q@gmail.com>
2018-10-30 16:23:24 +0500

    Base Using Controller class in main.cpp
    Removed start function in main.cpp, using run memfunc for control input

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,17 @@
 #include <ncurses.h>
 #include "view.h"
 #include "game.h"
-
-namespace
-{
-	void start()
-	{
-		OnlineGame::View view;
-		OnlineGame::Game game{view};
-
-		game.start();
-
-		while (true)
-		{
-			refresh();
-			int key = getch();
-			switch (key)
-			{
-				case KEY_DOWN	: game.move(1, 0);	break;
-				case KEY_UP	: game.move(-1, 0);	break;
-				case KEY_LEFT	: game.move(0, -1);	break;
-				case KEY_RIGHT	: game.move(0, 1);	break;
-				case 'q'	: return;
-			}
-		}
-	}
-}
+#include "controller.h"
 
 int main()
 {
 	initscr();
 	keypad(stdscr, true);
 	noecho();
-	start();
+	OnlineGame::View view;
+	OnlineGame::Game game{view};
+	OnlineGame::Controller controller{game};
+	controller.run();
 	endwin();
 
 	return 0;
