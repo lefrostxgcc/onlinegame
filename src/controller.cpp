2d6ee8632ea248913486ffc1aec226c39db16b51
Chip <chipz68q@gmail.com>
2018-10-30 16:21:59 +0500

    Implemented Controller::run memfun
    Moved implementation from start function in main.cpp to run memfun

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,10 +1,25 @@
 #include "controller.h"
+#include <ncurses.h>
 
 OnlineGame::Controller::Controller(const Game &g)
 	: game{g}
 {
+	game.start();
 }
 
 void OnlineGame::Controller::run()
 {
+	while (true)
+	{
+		refresh();
+		int key = getch();
+		switch (key)
+		{
+			case KEY_DOWN	: game.move(1, 0);	break;
+			case KEY_UP	: game.move(-1, 0);	break;
+			case KEY_LEFT	: game.move(0, -1);	break;
+			case KEY_RIGHT	: game.move(0, 1);	break;
+			case 'q'	: return;
+		}
+	}
 }
