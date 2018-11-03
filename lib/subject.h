077629627efda20fadac3ddbc5bbc24a608700ce
Chip <chipz68q@gmail.com>
2018-11-03 11:45:49 +0500

    Added enum Subject header

---
+++ lib/subject.h
@@ -0,0 +1,14 @@
+#ifndef ONLINEGAME_SUBJECT_H
+#define ONLINEGAME_SUBJECT_H
+namespace OnlineGame
+{
+	enum class Subject {
+		nulls = 0,
+		space = ' ',
+		block = '#',
+		user1 = '1',
+		user2 = '2',
+		money = '$'
+	};
+}
+#endif /* ONLINEGAME_SUBJECT_H */
