cf2379dc1a271090e55c34476a5bf58830df3767
Chip <chipz68q@gmail.com>
2018-11-08 22:13:54 +0500

    Added colors for user1 and block

--- src/view_color.cpp
+++ src/view_color.cpp
@@ -4,6 +4,8 @@
 OnlineGame::ViewColor::ViewColor()
 {
 	init_pair(1, COLOR_WHITE, COLOR_BLUE);
+	init_pair(2, COLOR_CYAN, COLOR_BLUE);
+	init_pair(3, COLOR_YELLOW, COLOR_BLUE);
 	attron(COLOR_PAIR(1));
 	bkgd(COLOR_PAIR(1));
 }
@@ -15,6 +17,10 @@ void OnlineGame::ViewColor::welcome()
 
 void OnlineGame::ViewColor::show(Coord coord, Subject s)
 {
+	if (s == Subject::block)
+		attron(COLOR_PAIR(2));
+	else if (s == Subject::user1)
+		attron(COLOR_PAIR(3));
 	mvaddch(coord.row, coord.col, static_cast<int>(s));
 }
 
