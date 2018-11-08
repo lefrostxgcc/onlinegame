4f1db4f74dc3903c2cd82a29923cfdb4c9b2d45d
Chip <chipz68q@gmail.com>
2018-11-08 22:05:47 +0500

    Added test for obstacles

--- lib/game.cpp
+++ lib/game.cpp
@@ -24,10 +24,18 @@ void OnlineGame::Game::move(int srow, int scol)
 	Coord move_coord{user1_coord.row + srow, user1_coord.col + scol};
 	if (!move_coord.in_size(level.get_size()))
 		return;
-	level.set_subject(user1_coord, Subject::space);
-	level.set_subject(move_coord, Subject::user1);
-	view->show(user1_coord, Subject::space);
-	view->show(move_coord, Subject::user1);
-	user1_coord = move_coord;
-	view->refresh();
+	Subject s = level.get_subject(move_coord);
+	if (s == Subject::nulls)
+		return;
+	else if (s == Subject::block)
+		return;
+	else if (s == Subject::space)
+	{
+		level.set_subject(user1_coord, Subject::space);
+		level.set_subject(move_coord, Subject::user1);
+		view->show(user1_coord, Subject::space);
+		view->show(move_coord, Subject::user1);
+		user1_coord = move_coord;
+		view->refresh();
+	}
 }
